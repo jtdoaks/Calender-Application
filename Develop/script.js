21e652e4932d38a8d7dfa9c9647ecbe94b1f4352
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-08-02 17:03:40 -0400

    corrected starter code

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,9 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  document.querySelector(".btn").addEventListener("click", function(){
+    localStorage.setItem("dayNotes", "")
+  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
